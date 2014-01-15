5708528646f9e2058d11f193f40e0e3eda4743f0
kaxla <kaylas.email.address@gmail.com>
2014-01-14 17:04:12 -0800

    update README

--- test/features/welcome_spec.rb
+++ test/features/welcome_spec.rb
@@ -4,7 +4,7 @@ require "test_helper"
 describe "my portfolio app welcome page" do
   it "shows Welcome index view" do
     visit "http://localhost:3000"
-    page.text.must_include "Welcome aboard"
+    page.text.must_include "Hello"
   end
 end
 
