81391cfd215a6cb664f0842be7664046e3e26872
kaxla <kaylas.email.address@gmail.com>
2014-01-21 13:06:13 -0800

    implement validation for new post form as well as tests for it

--- test/features/projects/create_project_test.rb
+++ test/features/projects/create_project_test.rb
@@ -12,3 +12,18 @@ feature "As the site owner, I want to add a portfolio item so that I can show of
     page.text.must_include "Rails"
   end
 end
+
+feature "As the site owner, I went to add a portfolio" do
+    scenario "new project has invalid data" do
+    #Given invalid project data is submitted to the form
+    visit new_project_path
+    # WHen the form is submitted with a short name and missing technologies used section
+    fill_in "Name", with: "a"
+    click_on "Create Project"
+    # Then the form should re-display with an error message.
+    current_path.must_match /projects$/
+    page.text.must_include "Project could not be saved"
+    page.text.must_include "Name is too short"
+    page.text.must_include "Technologies used can't be blank"
+  end
+end
