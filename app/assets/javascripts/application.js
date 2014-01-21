d4cc49096dd06eab91afb81c7796ac5d50f9199e
kaxla <kaylas.email.address@gmail.com>
2014-01-20 16:14:25 -0800

    fix up asset files and index files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+// require foundation
 //= require turbolinks
 //= require_tree .
+<<<<<<< HEAD
 
+=======
+>>>>>>> clean slate
 $(document).foundation();
