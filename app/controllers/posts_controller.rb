e05a8c24edd87710bdc7749e3bbb8e337f369f09
kaxla <kaylas.email.address@gmail.com>
2014-02-11 11:04:11 -0800

    ajaxified destroy and adding post to index in page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,7 +69,7 @@ class PostsController < ApplicationController
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url }
-      format.json { head :no_content }
+      format.js
     end
   end
 
