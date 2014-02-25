02679f0937e5882fcec7ce703143c4e7089d43eb
kaxla <kaylas.email.address@gmail.com>
2014-02-24 20:06:02 -0800

    add test to mailer

--- test/mailers/mails_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class MailsMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/mailers/notifications_mailer_test.rb
+++ test/mailers/notifications_mailer_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
-class NotificationsMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+feature 'Contact page' do
+  scenario 'the email form works' do
+    visit contact_path
+    fill_in "Name", with: "Bob Jones"
+    fill_in "Email", with: "bobjones@bobjones.com"
+    fill_in "Subject", with: "This is Bob Jones"
+    fill_in "Body", with: "Bob Jones"
+    click_button "Send"
+    page.text.must_include "Hi My Name Is Kayla Morrison"
+  end
 end
